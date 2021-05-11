f8a48692dddbbbe650ed67b0d0d2a5748b903d96
nikkajayme <69382283+nikkajayme@users.noreply.github.com>
2021-05-11 10:04:07 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,11 +14,13 @@ const mobile_menu = () => {
 mobile_menu();
 
 const mobile_menu_links = () => {
+	const mediaQuery = window.matchMedia('(max-width: 640px)');
 	const burger = document.querySelector('.hamburger');
 	const mLinks = document.querySelectorAll('.m-link');
 	const menu = document.querySelector('.menu');
 	const body = document.querySelector('.scroll');
 	const hide = document.querySelector('.hide');
+	if (mediaQuery.matches) {
 		mLinks.forEach(function(mLink) {
 			mLink.addEventListener('click', function() {
 				console.log('m-link clicked');
@@ -28,6 +30,8 @@ const mobile_menu_links = () => {
 				burger.classList.toggle('toggle');
 			});
 		});
+	}
+		
 };
 
 mobile_menu_links();
